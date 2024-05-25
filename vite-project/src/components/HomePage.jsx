d5ec67c1eed7de65eb6d910af6a79ce31aafb3f2
NicollyNOA <152316760+NicollyNOA@users.noreply.github.com>
2024-05-24 23:54:25 -0300

    Tela Inicial
    Mexi em umas coisinhas

--- vite-project/src/components/HomePage.jsx
+++ vite-project/src/components/HomePage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const [page, setPage] = useState("home");
-  const [activePage, setActivePage] = useState("home");
   const navigate = useNavigate();
 
   const renderPage = () => {
@@ -37,12 +36,16 @@ export default function HomePage() {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-200">
-      <div className="h-3/5 w-4/5 flex flex-row items-center justify-evenly rounded-3xl bg-blue-100">
-        <div className="w-48 h-48">
+    <div className="h-screen flex items-center justify-center custom-background">
+      <div className="absolute top-10 text-2xl font-bold">
+        Página Inicial ImobSystem
+      </div>
+      <div className="h-3/5 w-4/5 flex flex-row items-center justify-evenly rounded-3xl custom-rectangle">
+        {/* Criar Novo Contrato */}
+        <div className="box w-48 h-48 flex items-center justify-center border rounded-lg shadow-lg">
           <button
             onClick={() => document.getElementById("my_modal_3").showModal()}
-            className="w-full h-full"
+            className="w-full h-full flex flex-col items-center justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +53,7 @@ export default function HomePage() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className=""
+              className="w-12 h-12"
             >
               <path
                 strokeLinecap="round"
@@ -58,22 +61,14 @@ export default function HomePage() {
                 d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
               />
             </svg>
-            <h1>Criar Novo Contrato</h1>
-            <dialog id="my_modal_3" className="modal">
-              <div className="w-full">
-                <p className="py-4">
-                  <FormNewContract />
-                </p>
-              </div>
-            </dialog>
+            <h1 className="mt-2 text-center">Criar Novo Contrato</h1>
           </button>
         </div>
-        <div className="w-48 h-48">
+        {/* Abrir Contratos */}
+        <div className="box w-48 h-48 flex items-center justify-center border rounded-lg shadow-lg">
           <button
-            onClick={() => {
-              navigate("/ActiveContracts");
-            }}
-            className="w-full h-full"
+            onClick={() => navigate("/ActiveContracts")}
+            className="w-full h-full flex flex-col items-center justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -81,7 +76,7 @@ export default function HomePage() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className=""
+              className="w-12 h-12"
             >
               <path
                 strokeLinecap="round"
@@ -89,15 +84,14 @@ export default function HomePage() {
                 d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
               />
             </svg>
-            <h1>Abrir Contratos</h1>
+            <h1 className="mt-2 text-center">Abrir Contratos</h1>
           </button>
         </div>
-        <div className="w-48 h-48">
+        {/* Contratos finalizados */}
+        <div className="box w-48 h-48 flex items-center justify-center border rounded-lg shadow-lg">
           <button
-            onClick={() => {
-              navigate("/terminated");
-            }}
-            className="w-full h-full"
+            onClick={() => navigate("/terminated")}
+            className="w-full h-full flex flex-col items-center justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -105,7 +99,7 @@ export default function HomePage() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className=""
+              className="w-12 h-12"
             >
               <path
                 strokeLinecap="round"
@@ -113,15 +107,14 @@ export default function HomePage() {
                 d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
               />
             </svg>
-            <h1>Contratos finalizados</h1>
+            <h1 className="mt-2 text-center">Contratos finalizados</h1>
           </button>
         </div>
-        <div className="w-48 h-48">
+        {/* Notificações */}
+        <div className="box w-48 h-48 flex items-center justify-center border rounded-lg shadow-lg">
           <button
-            onClick={() => {
-              navigate("/notifications");
-            }}
-            className="w-full h-full"
+            onClick={() => navigate("/notifications")}
+            className="w-full h-full flex flex-col items-center justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -129,32 +122,20 @@ export default function HomePage() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
+              className="w-12 h-12"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v-1.5A2.25 2.25 0 0 0 19.5 3h-15A2.25 2.25 0 0 0 2.25 5.25v1.5m0 0A2.25 2.25 0 0 1 4.5 3h15a2.25 2.25 0 0 1 2.25 2.25V6.75"
               />
             </svg>
-
-            <h1>Notificações</h1>
-            <dialog id="my_modal_3" className="modal">
-              <div className="modal-box">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                    ✕
-                  </button>
-                </form>
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">
-                  Press ESC key or click on ✕ button to close
-                </p>
-              </div>
-            </dialog>
+            <h1 className="mt-2 text-center">Notificações</h1>
           </button>
         </div>
       </div>
     </div>
   );
 }
+
+
